4a2df43f9d37280b2d2e6f26d5925c622963d20f
Teddy Blomgren <Teddy_95@live.se>
2024-10-24 09:51:00 +0200

    Added Prison

--- TjuvOchPolis/Prison.cs
+++ TjuvOchPolis/Prison.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace TjuvOchPolis
 {
-    internal class Fängelse
+    internal class Prison
     {
+        public static void PrintPrison()
+        {
+            int width = 25;
+            int height = 10;
+
+            for (int i = 0; i < height; i++)
+            {
+
+                if (i == 0)
+                {
+                    //Console.Write("Fängelse");
+
+                    for (int k = 0; k < width; k++)
+                    {
+                        Console.Write("=");
+                    }
+                    Console.WriteLine();
+                }
+                else if (i == height - 1)
+                {
+
+                    Console.Write("Status");
+
+
+                    for (int j = 6; j < width - 1; j++)
+                    {
+                        Console.Write("=");
+                    }
+
+                    Console.Write(" ");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write("|");
+
+                    for (int j = 1; j < width - 1; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    Console.Write("|");
+
+                    Console.WriteLine();
+                }
+            }
+        }
     }
 }
