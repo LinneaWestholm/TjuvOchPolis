290140c050bed83481476d948c80ec8b0e761ee7
Teddy Blomgren <Teddy_95@live.se>
2024-10-24 09:42:16 +0200

    Added City and prison

---
+++ TjuvOchPolis/City.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjuvOchPolis
+{
+    internal class City
+    {
+        public static void Print()
+        {
+            int width = 100;
+            int height = 26;
+
+            for (int i = 0; i < height; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write("City");
+
+                    for (int k = 4; k < width; k++)
+                    {
+                        Console.Write("=");
+                    }
+                    Console.WriteLine();
+                }
+                else if (i == height - 2)
+                {
+
+                    Console.Write("Fängelse");
+
+
+                    for (int j = 8; j < width - 1; j++)
+                    {
+                        Console.Write("=");
+                    }
+
+                    Console.Write(" ");
+                    Console.WriteLine();
+                }
+                else if (i == height - 1)
+                {
+                }
+                else
+                {
+                    Console.Write("X");
+
+                    for (int j = 1; j < width - 1; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    Console.Write("X");
+                    Console.WriteLine();
+
+                }
+            }
+            Prison.PrintPrison();
+        }
+    }
+}
