7fdb6682d70900212d0a809a600c22b513b32a58
Pontus Eriksson <pontus.eriksson@nykopingcampus.se>
2024-10-29 11:03:39 +0100

    added civilian inventory

--- TjuvOchPolis/Civilian.cs
+++ TjuvOchPolis/Civilian.cs
@@ -12,8 +12,8 @@ namespace TjuvOchPolis
 
         public Medborgare(int positionY, int positionX, int directionX, int directionY ) : base(positionX, positionY, directionX, directionY, 'C')
         {
-            
 
+            Inventory.AddRange(new List<string> { "Nycklar", "Klocka", "Mobiltelefon", "Pengar" });
 
         }
 
