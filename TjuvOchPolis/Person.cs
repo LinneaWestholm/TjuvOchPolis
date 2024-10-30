7ab856ce53fca052f6598c5a41e1f1a07a4614f2
Teddy Blomgren <Teddy_95@live.se>
2024-10-30 14:49:33 +0100

    fixed merge prob

--- TjuvOchPolis/Person.cs
+++ TjuvOchPolis/Person.cs
@@ -48,46 +48,22 @@ namespace TjuvOchPolis
         }
         public void Movement()
         {
-
             if (PositionX < 100)
             {
-                if (PositionX >= 98)
-                {
-                    PositionX = 1;
-                }
-                if (PositionX <= 0)
-                {
-                    PositionX = 98;
-                }
-                if (PositionY == 24)
-                {
-                    PositionY = 1;
-                }
-                if (PositionY <= 0)
-                {
-                    PositionY = 23;
-                }
+                if (PositionX >= 98) PositionX = 1;
+                else if (PositionX <= 0) PositionX = 98;
             }
-            if (PositionX > 100)
+            else 
             {
-                if (PositionX >= 134)
-                {
-                    PositionX = 111;
-                }
-                if (PositionX <= 110)
-                {
-                    PositionX = 134;
-                }
-                if (PositionY >= 24)
-                {
-                    PositionY = 1;
-                }
-                if (PositionY <= 0)
-                {
-                    PositionY = 23;
-                }
+                if (PositionX >= 134) PositionX = 111;
+                else if (PositionX <= 110) PositionX = 134;
             }
 
+            
+            if (PositionY >= 24) PositionY = 1;
+            else if (PositionY <= 0) PositionY = 23;
+
+            
             Console.SetCursorPosition(PositionX, PositionY);
             Console.Write(Title);
         }
